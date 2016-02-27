b389a80704a9c63cab3af7fe0ac0ac37359163ea
Joshua Adadevoh <joshuaada@citrite.net>
2016-02-26 23:35:35 -0500

    wrapped script into copy_file function

--- Copy_Delete.py
+++ Copy_Delete.py
@@ -5,24 +5,25 @@ print "This is a simple file copy/deletion tool"
 
 #copy .... 
 
-print "Enter path to file to copy from"
-print "If same directory as script just enter 'file_name.extension' "
-copy_from_file  = raw_input("Copy from : ")
-from_file = open (copy_from_file)
-data = from_file.read()
-print len(data)
+def copy_file():
+    print "Enter path to file to copy from"
+    print "If same directory as script just enter 'file_name.extension' "
+    from_file_name  = raw_input("Copy from : ")
+    from_file = open (from_file_name)
+    data = from_file.read()
+    print "size in bytes to be copied: ", len(data)
 
 
-if not exists(copy_from_file):
-    print "sorry that file does not exist"
-else:
-    print "\n"
+    if not exists(from_file_name):
+        print "sorry that file does not exist"
+    else:
+        print "\n"
 
-    print "Enter path of file to copy to "
-    dest_file = raw_input("Copy to : ")
-    to = open(dest_file, "w+")
-    to.writelines(data)
+        print "Enter path of file to copy to "
+        dest_file = raw_input("Copy to : ")
+        to = open(dest_file, "w+")
+        to.writelines(data)
 
 
-to.close()
-from_file.close()
+    to.close()
+    from_file.close()
